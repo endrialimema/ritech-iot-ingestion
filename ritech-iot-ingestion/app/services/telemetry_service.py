942e88802ffda1e8ad01bef9954bf72b57677315
endrialimema <endrialimema976@gmail.com>
2026-04-27 16:55:35 +0200

    Refactor create_telemetry to manage time buckets

--- ritech-iot-ingestion/app/services/telemetry_service.py
+++ ritech-iot-ingestion/app/services/telemetry_service.py
@@ -1,10 +1,68 @@
-from typing_extensions import Literal
-
+from datetime import datetime, timedelta
 from ..db.mongodb import db
 
-async def create_telemetry(data):    
-    
+
+BUCKET_SIZE_SECONDS = 60
+
+
+def get_bucket_time(now: datetime):
+    bucket_start = now.replace(second=0, microsecond=0)
+    bucket_end = bucket_start + timedelta(seconds=BUCKET_SIZE_SECONDS)
+    return bucket_start, bucket_end
+
+
+async def create_telemetry(data):
+
     print("INSERT CALLED", data.dict())
-    result = await db.telemetry.insert_one(data.dict())
-    print("INSERT DONE", result.inserted_id)
-    return {"id": str(result.inserted_id)}
+
+    MAX_BUCKET_SIZE = 5
+
+    now = datetime.utcnow()
+    bucket_start, bucket_end = get_bucket_time(now)
+
+    reading = {
+        "ts": now,
+        "value": data.value
+    }
+
+    query = {
+        "device_id": data.device_id,
+        "bucket_start": bucket_start
+    }
+    
+    existing_bucket = await db.telemetry.find_one(query)
+
+    if existing_bucket and len(existing_bucket.get("readings", [])) >= MAX_BUCKET_SIZE:
+    # force new bucket by shifting time
+      now = datetime.utcnow()
+      bucket_start, bucket_end = get_bucket_time(now + timedelta(seconds=60))
+
+    query = {
+        "device_id": data.device_id,
+        "bucket_start": bucket_start
+    }
+
+    update = {
+        "$setOnInsert": {
+            "device_id": data.device_id,
+            "bucket_start": bucket_start,
+            "bucket_end": bucket_end,
+            #"readings": []
+            },
+        "$push": {
+            "readings": reading
+        }
+    }
+
+    result = await db.telemetry.update_one(
+        query,
+        update,
+        upsert=True
+    )
+
+    print("BUCKET UPDATED")
+
+    return {
+        "status": "ok",
+        "device_id": data.device_id
+    }
