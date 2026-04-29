8d1c4ffcf050307b636a0838db3dc509702178da
endrialimema <endrialimema976@gmail.com>
2026-04-29 15:06:47 +0200

    Fix initialization method name in TelemetryFSM

--- ritech-iot-ingestion/app/core/fsm.py
+++ ritech-iot-ingestion/app/core/fsm.py
@@ -1,10 +1,10 @@
+import json
+
 class TelemetryFSM:
-    def __init__(self):
+    def _init_(self):
         self.state = "RECEIVED"
 
     def parse(self, payload_raw):
-        import json
-
         try:
             data = json.loads(payload_raw)
             self.state = "PARSED"
@@ -19,7 +19,6 @@ class TelemetryFSM:
             return False
         
         required_fields = ["device_id", "value"]
-
         for field in required_fields:
             if field not in data:
                 self.state = "REJECTED"
@@ -28,7 +27,6 @@ class TelemetryFSM:
         if not isinstance(data["device_id"], str):
             self.state = "REJECTED"
             return False
-
 
         if not isinstance(data["value"], (int, float)):
             self.state = "REJECTED"
