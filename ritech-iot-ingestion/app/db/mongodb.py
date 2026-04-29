1000c704ddd27c889210583ef7883992046af5f6
endrialimema <endrialimema976@gmail.com>
2026-04-29 15:05:25 +0200

    Refactor MongoDB connection handling
    Updated MongoDB connection logic to fail safely if the environment variable is missing.

--- ritech-iot-ingestion/app/db/mongodb.py
+++ ritech-iot-ingestion/app/db/mongodb.py
@@ -3,27 +3,18 @@ import os
 import time
 
 def get_db():
-    mongo_url = os.getenv(
-        "MONGO_URL",
-        "mongodb://mongo-raw-ingestion:27017/admin"
-    )
+    # Fails safely if the environment variable is missing
+    mongo_url = os.environ["MONGO_URL"]
 
     for _ in range(10):
         try:
-            print("MONGO URL:", mongo_url)
-
             client = AsyncIOMotorClient(
                 mongo_url,
                 serverSelectionTimeoutMS=2000
             )
-
-            client.server_info()  # force connection check
-
-            print("Mongo connected")
+            client.server_info()
             return client["telemetry_db"]
-
         except Exception as e:
-            print("Mongo not ready, retrying...", e)
             time.sleep(2)
 
     raise Exception("Failed to connect to MongoDB after 10 retries")
